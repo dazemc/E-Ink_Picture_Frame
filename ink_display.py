5de2ca2513562c1c7b3f20462ddd8132df8a0c72
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-04-22 14:10:05 -0700

    draw implemented fully

--- ink_display.py
+++ ink_display.py
@@ -4,51 +4,62 @@ from globals import *
 import logging
 from lib import epd7in3f
 import time
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image, ImageDraw, ImageFont
 
 logging.basicConfig(level=logging.DEBUG)
+
 
 class InkDisplay:
     def __init__(self) -> None:
         self.ink = epd7in3f.EPD()
-        self.fonts = {}
-        
-        if os.path.exists(LIB_DIR):
-            sys.path.append(LIB_DIR)
-    
+        self.Himage_draw = Image.new(
+            "RGB", (self.ink.width, self.ink.height), self.ink.WHITE
+        )
+        self.draw = ImageDraw.Draw(self.Himage_draw)
+
     def init(self) -> None:
         logging.info("initialising")
+        if os.path.exists(LIB_DIR):
+            sys.path.append(LIB_DIR)
         self.ink.init()
-    
+
     def clear(self) -> None:
         logging.info("clearing screen")
         self.ink.Clear()
-        
+
     def sleep(self) -> None:
         logging.info("putting to sleep")
         self.ink.sleep()
-    
+
     def exit(self) -> None:
         logging.info("ctrl + c:")
         epd7in3f.epdconfig.module_exit()
-        
-    def font(self, font, size) -> None:
-        self.fonts[size] = ImageFont.truetype(os.path.join(FONT_DIR, font), size)
 
-        
     def display_image(self, image) -> None:
         logging.info("displaying image")
         Himage = Image.open(os.path.join(IMG_DIR, image))
         self.ink.display(self.ink.getbuffer(Himage))
         logging.info("sleeping for 15 seconds")
         time.sleep(15)
+        self.font
 
-    def display_text(self, text, location, size, color):
-        Himage = Image.new('RGB', (self.ink.width, self.ink.height), self.ink.WHITE)
-        draw = ImageDraw.Draw(Himage)
-        draw.text((location), text, font=self.fonts[size], fill=color)
-        self.ink.display(self.ink.getbuffer(Himage))
+    def clear_draw(self) -> None:
+        logging.info("creating new draw image")
+        self.Himage_draw = Image.new(
+            "RGB", (self.ink.width, self.ink.height), self.ink.WHITE
+        )
+        self.draw = ImageDraw(self.Himage_draw)
+
+    def draw_text(self, location, text, font, size, color) -> None:
+        logging.info(f"writing text to draw image: {text}")
+        self.draw.text(
+            (location),
+            text,
+            ImageFont.truetype(os.path.join(FONT_DIR, font), size),
+            color,
+        )
         time.sleep(3)
-        
-    def display_draw(self):
-        pass    
+
+    def display_draw(self) -> None:
+        logging.info("displaying draw image")
+        self.ink.display(self.ink.getbuffer(self.Himage_draw))
--- main.py
+++ main.py
@@ -4,34 +4,48 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 ink = ink.InkDisplay()
-image = 'zelda00.bmp'
-font = 'Font.ttc'
+image = "zelda00.bmp"
+font = "Font.ttc"
+
 
 def main():
     try:
         # INIT/CLEAR
         ink.init()
         ink.clear()
-        
+
         # DISPLAY IMAGE
-        ink.display_image(image)
+        # ink.display_image(image)
+        # ink.clear()
+
+        # CREATE DRAW
+        ink.draw_text(
+            text="hello", location=(5, 0), font=font, size=24, color="#FF0000"
+        )  # Must be font size that has been set above
+        ink.draw_text(
+            text="world", location=(5, 15), font=font, size=16, color="#FF0000"
+        )
+        ink.draw.line((5, 170, 80, 245), fill="#0000FF")
+        ink.display_draw()
         ink.clear()
-        
-        # DISPLAY FONT
-        ink.font(font, 24)
-        ink.display_text(text="hello world", location=(5, 0), size=24, color="#FF0000")
+
+        # CREATE NEW DRAW
+        ink.clear_draw()
+        ink.draw_text(
+            text="goodbye world", location=(5, 0), font=font, size=36, color="#00FF00"
+        )
+        ink.display_draw()
         ink.clear()
-        
-        # DISPLAY DRAW
-        # TODO: implement draw
-        
+
+        # SLEEP
         ink.sleep()
-                    
+
     except IOError as e:
         logging.info(e)
-        
-    except KeyboardInterrupt:    
+
+    except KeyboardInterrupt:
         ink.exit()
         exit()
+
 
 main()
